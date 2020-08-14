265382a6a20349f95bf734de143b9002a0971a32
Brian Levin <bml201095@gmail.com>
2020-08-14 01:45:20 -0400

    created a regular expression for the phone number

--- app.js
+++ app.js
@@ -37,3 +37,15 @@ function validateZip() {
       email.classList.remove('is-invalid');
     }
   }
+
+  
+function validatePhone() {
+    const phone = document.getElementById('phone');
+    const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+  
+    if(!re.test(phone.value)){
+      phone.classList.add('is-invalid');
+    } else {
+      phone.classList.remove('is-invalid');
+    }
+  }
